7a0602d2f9b9f50555039a7c68430163adb6f2a2
Axel LAPLACE <axellaplace1709@gmail.com>
2024-07-24 11:02:28 +0200

    ecercice 9 completed

--- app/training/[...path]/exercices/4.use-effect/9.jsx
+++ app/training/[...path]/exercices/4.use-effect/9.jsx
@@ -1,14 +1,54 @@
 "use client";
 
-import { useState } from "react";
+import { useRef } from "react";
+import { useState, useEffect } from "react";
+
+const handlerSet = new Set();
+
+const useEventListener = ({
+  eventName,
+  handler,
+  element = window,
+  enabled = true,
+}) => {
+  const handlerRef = useRef(handler);
+
+  useEffect(() => {
+    handlerRef.current = handler;
+  }, [handler]);
+
+  handlerSet.add(handler);
+  console.log(handlerSet);
+  console.log(handlerSet.size);
+  useEffect(() => {
+    if (!enabled) return;
+    const incrementCount = () => {
+      handlerRef.current();
+    };
+    console.log("addEventListener");
+    element.addEventListener(eventName, incrementCount);
+
+    return () => {
+      console.log("removeEventListener");
+
+      element.removeEventListener(eventName, incrementCount);
+    };
+  }, [enabled, element, eventName]);
+};
 
 export default function App() {
   const [isCountingClick, setIsCountingClick] = useState(false);
   // eslint-disable-next-line no-unused-vars
   const [count, setCount] = useState(0);
 
-  // 🦁 Créer un `useEffect` qui vient écouter les click sur `window`
-  // 🦁 Ensuite il incrémente le state `count` uniquement si `isCountingClick` est `true`
+  useEventListener({
+    eventName: "click",
+    handler: () => {
+      setCount((curr) => curr + 1);
+    },
+    element: window,
+    enabled: isCountingClick,
+  });
 
   return (
     <div className="flex max-w-sm flex-col gap-8">
