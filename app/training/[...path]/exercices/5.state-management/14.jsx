56fd7aca6951c15d2dfd51cf33f1db2bd91b726e
Axel LAPLACE <axellaplace1709@gmail.com>
2024-08-02 12:58:14 +0200

    Exercice 14 completed

--- app/training/[...path]/exercices/5.state-management/14.jsx
+++ app/training/[...path]/exercices/5.state-management/14.jsx
@@ -1,34 +1,53 @@
 "use client";
 
 import { cn } from "@/src/utils/cn";
+import { create } from "zustand";
 import { Check, ShoppingBasket } from "lucide-react";
-import { useState } from "react";
+import { useState, useContext, createContext } from "react";
 
-// 🦁 Créer un context avec les informations suivant :
-// - `items` : un tableau de type `{ id: number, name: string, cover: string }`
-// - `onDeleteItem` : une fonction qui va supprimer un item
-// - `onAddItem` : une fonction qui va ajouter un item dans le panier
+const useBasketStore = create((set) => ({
+  items: [],
 
-// 🦁 Supprime les imports et utilise le context
-const Header = ({ items, onDeleteItem }) => {
+  onAddItem: (item) =>
+    set((state) => ({
+      items: [...state.items, item],
+    })),
+
+  onDeleteItem: (item) =>
+    set((state) => ({
+      items: state.items.filter((i) => i.id !== item.id),
+    })),
+}));
+
+const Header = () => {
+  const { items, onAddItem, onDeleteItem } = useBasketStore();
   return (
     <div className="flex items-center justify-between rounded-lg border border-neutral/40 bg-base-200 px-8 py-4 shadow-lg">
       <h2 className="text-2xl font-bold">Shoes</h2>
-      <div className="dropdown">
-        <button tabIndex={0} className="btn btn-secondary m-1">
-          <ShoppingBasket size={16} /> {items.length}
-        </button>
-        <div
-          tabIndex={0}
-          className="card dropdown-content card-compact z-[1] w-64 bg-base-200 p-2 shadow"
-        >
-          <ul>
-            {items.length === 0 ? (
-              <li className="flex w-60 flex-row flex-nowrap items-center justify-between p-1">
-                No items
-              </li>
-            ) : null}
-            {items.map((item, index) => (
+      <BasketButton />
+    </div>
+  );
+};
+
+const BasketButton = () => {
+  const items = useBasketStore((state) => state.items);
+  const onDeleteItem = useBasketStore((state) => state.onDeleteItem);
+  return (
+    <div className="dropdown">
+      <button tabIndex={0} className="btn btn-secondary m-1">
+        <ShoppingBasket size={16} /> {items.length}
+      </button>
+      <div
+        tabIndex={0}
+        className="card dropdown-content card-compact z-[1] w-64 bg-base-200 p-2 shadow"
+      >
+        <ul>
+          {items.length === 0 ? (
+            <li className="flex w-60 flex-row flex-nowrap items-center justify-between p-1">
+              No items
+            </li>
+          ) : (
+            items.map((item, index) => (
               <li
                 key={index}
                 className="flex w-60 flex-row flex-nowrap items-center justify-between p-1"
@@ -48,9 +67,9 @@ const Header = ({ items, onDeleteItem }) => {
                   Delete
                 </button>
               </li>
-            ))}
-          </ul>
-        </div>
+            ))
+          )}
+        </ul>
       </div>
     </div>
   );
@@ -58,43 +77,20 @@ const Header = ({ items, onDeleteItem }) => {
 
 export default function App() {
   // 🦁 Déplace le state dans un context
-  const [items, setItems] = useState([]);
 
   return (
     <div className="flex flex-col gap-8">
-      <Header
-        // 🦁 Enlève les props
-        onDeleteItem={(item) => {
-          setItems((prevItems) => prevItems.filter((i) => i.id !== item.id));
-        }}
-        items={items}
-      />
+      <Header />
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
         {SHOES.map((shoe) => (
-          <ShoeCard
-            // 🦁 Enlève les props
-            isSelected={items.some((item) => item.id === shoe.id)}
-            onShoppingBasketClick={() => {
-              console.log("click", shoe);
-              if (items.some((item) => item.id === shoe.id)) {
-                setItems((prevItems) =>
-                  prevItems.filter((item) => item.id !== shoe.id)
-                );
-              } else {
-                setItems((prevItems) => [...prevItems, shoe]);
-              }
-            }}
-            key={shoe.id}
-            shoe={shoe}
-          />
+          <ShoeCard key={shoe.id} shoe={shoe} />
         ))}
       </div>
     </div>
   );
 }
 
-const ShoeCard = ({ shoe, isSelected = false, onShoppingBasketClick }) => {
-  // 🦁 Utilise le context pour récupérer le state `isSelected` ainsi que `onAddItem` et `onDeleteItem`
+const ShoeCard = ({ shoe }) => {
   return (
     <div className="card flex w-full bg-base-300 shadow-xl">
       <figure>
@@ -107,20 +103,39 @@ const ShoeCard = ({ shoe, isSelected = false, onShoppingBasketClick }) => {
       <div className="card-body">
         <h2 className="card-title">{shoe.name}</h2>
         <div className="card-actions flex w-full items-end justify-end">
-          <button
-            // En fonction de `useSelected` on ajoutera ou supprimera du panier
-            onClick={onShoppingBasketClick}
-            className={cn("btn", {
-              "btn-outline": isSelected,
-              "btn-primary": !isSelected,
-            })}
-          >
-            <ShoppingBasket size={16} />{" "}
-            {isSelected ? <Check size={16} /> : null}
-          </button>
+          <ShoeCardButtons shoe={shoe} />
         </div>
       </div>
     </div>
+  );
+};
+
+const ShoeCardButtons = ({ shoe }) => {
+  const onAddItem = useBasketStore((state) => state.onAddItem);
+  const onDeleteItem = useBasketStore((state) => state.onDeleteItem);
+  const isSelected = useBasketStore((state) =>
+    state.items.some((item) => item.id === shoe.id)
+  );
+
+  const handleBasketClick = () => {
+    if (!isSelected) {
+      onAddItem(shoe);
+    } else {
+      onDeleteItem(shoe);
+    }
+  };
+
+  return (
+    <button
+      // En fonction de `useSelected` on ajoutera ou supprimera du panier
+      onClick={handleBasketClick}
+      className={cn("btn", {
+        "btn-outline": isSelected,
+        "btn-primary": !isSelected,
+      })}
+    >
+      <ShoppingBasket size={16} /> {isSelected ? <Check size={16} /> : null}
+    </button>
   );
 };
 
